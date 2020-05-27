1d578b2a971b5bf2d6ce9c3e60f5b78dd13bf85f
Benjamin Soltoff <ben@bensoltoff.com>
2020-05-27 13:19:15 -0500

    Include Twitter API key and token - DEACTIVATE AFTER CLASS

--- 01-twitter-solution.R
+++ 01-twitter-solution.R
@@ -1,14 +1,20 @@
 library(tidyverse)
 library(rtweet)
 
-# Authenticate access to the Twitter API using the instructions
-# I post in class.
+# Authenticate access to the Twitter API using the token
+# provided for class today
+api_key <- "2chBhBGKCAjlPkt7ZCA2aBXvu"
+api_secret_key <- "ezjmyz4mJN5gRIALpV1q3WUZTu5xtkUGPkhNhJnhDFZBNYhFiT"
+access_token <- "3234588499-gbFL4E2POWGUBR9IDl9zII459ZhyN8u4GGFB97U"
+access_token_secret <- "ppptLqs6dJaCgPqqLF93YWasiKOibcgJMeVUv1DMhVeEo"
+
+## store authentication information
 token <- create_token(
-  app = "XXXXXXXXXXXXXXX",
-  consumer_key = "XXXXXXXXXXXXXXX",
-  consumer_secret = "XXXXXXXXXXXXXXX",
-  access_token = "XXXXXXXXXXXXXXX",
-  access_secret = "XXXXXXXXXXXXXXX"
+  app = "CFSS API test app",
+  consumer_key = api_key,
+  consumer_secret = api_secret_key,
+  access_token = access_token,
+  access_secret = access_token_secret
 )
 
 # Find the 1000 most recent tweets by
